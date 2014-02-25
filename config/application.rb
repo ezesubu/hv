7e356113b29f558b4d9c55226f7a780edd003c06
Ezequiel Suárez <ezesubu@gmail.com>
2014-02-25 00:18:43 -0500

    assets

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Hv
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets"
+    
+
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
   end
 end
